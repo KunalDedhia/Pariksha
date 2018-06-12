0f6cfc7896950e501cf6ba5996171040b1ef8d6f
KunalDedhia <38349685+KunalDedhia@users.noreply.github.com>
2018-06-13 05:27:16 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pariksha.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
